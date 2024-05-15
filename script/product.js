65f7abfb5d741c8259cec093a2f926516bbe5703
lizonkisel <ees.hwork@gmail.com>
2024-05-15 21:29:04 +0300

    feat: add images to gallery at the products page

--- script/product.js
+++ script/product.js
@@ -3,6 +3,7 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper(".mySwiper", {
+  centeredSlides: true,
   slidesPerView: 1,
   loop: true,
   spaceBetween: 30,
