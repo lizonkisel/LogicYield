6b8390229498cd75479e1b935b160f1568488c26
lizonkisel <ees.hwork@gmail.com>
2024-09-13 13:56:58 +0300

    feat: add texts translation for Soft page. Add languages buttons for every page

--- texts/softTexts.js
+++ texts/softTexts.js
@@ -1,11 +1,41 @@
 const softTexts = {
-  "dd": {
-		ru: "",
-		en: "",
+  "title": {
+		ru: "Софт",
+		en: "Software",
 	},
-  "dd": {
-		ru: "",
-		en: "",
+  "card-title": {
+		"1": {
+			ru: "DotPulse",
+			en: "DotPulse",
+		},
+		"2": {
+			ru: "DotPulse",
+			en: "DotPulse",
+		},
+		"3": {
+			ru: "GOA API",
+			en: "GOA API",
+		},
+		"4": {
+			ru: "GOA API",
+			en: "GOA API",
+		},
+		"5": {
+			ru: "Нейросетевой анализ номерных знаков автомобилей",
+			en: "Neural network analysis of vehicle license plates",
+		},
+		"6": {
+			ru: "Нейросетевой анализ номерных знаков автомобилей",
+			en: "Neural network analysis of vehicle license plates",
+		},
+		"7": {
+			ru: "Нейросетевой анализ и мониторинг спутниковых снимков",
+			en: "Neural network analysis and satellite image monitoring",
+		},
+		"8": {
+			ru: "Нейросетевой анализ и мониторинг спутниковых снимков",
+			en: "Neural network analysis and satellite image monitoring",
+		},
 	},
 }
 
