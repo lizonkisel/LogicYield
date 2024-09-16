9ffd21bad7d8801da732169198d12840ef1b1d2b
lizonkisel <ees.hwork@gmail.com>
2024-09-16 17:54:51 +0300

    feat: add english texts for Contacts page

--- texts/projectsTexts.js
+++ texts/projectsTexts.js
@@ -1,11 +1,67 @@
 const projectTexts = {
-  "dd": {
-		ru: "",
-		en: "",
+  "title": {
+		ru: "Перспективные проекты",
+		en: "Promising projects",
 	},
-  "dd": {
-		ru: "",
-		en: "",
+  "subtitle": {
+		ru: "Сейчас мы активно работаем над рядом проектов:",
+		en: "We are currently actively working on a number of projects:",
+	},
+  "projectItem-title": {
+		"1": {
+			ru: "Облачная инфраструктура для накопления и анализа промышленных интеллектуальных систем “Лоджик Илд”",
+			en: "Cloud Infrastructure for the Accumulation and Analysis of Industrial Intelligent Systems “Logic Yield”"
+		},
+		"2": {
+			ru: "API инфраструктура для любых создаваемых нейронных сетей",
+			en: "API infrastructure for any created neural networks"
+		},
+		"3": {
+			ru: "Устройство “UVYield” для анализа сыпучих материалов под ультрафиолетовым излучением",
+			en: "UVYield device for analyzing bulk materials under ultraviolet radiation"
+		}
+	},
+	"cloud-item": {
+		"1": {
+			ru: "Создание облачной инфраструктуры на основе базы данных (PostgreSQL), API (FastAPI) и интерфейса пользователя (RestAPI).",
+			en: "Creation of a cloud infrastructure based on a database (PostgreSQL), API (FastAPI), and user interface (RestAPI)."
+		},
+		"2": {
+			ru: "Бесшовное подключение всех устройств Лоджик Илд к облачной инфраструктуре по защищенному каналу за корпоративным VPN.",
+			en: "Seamless connection of all “Logic Yield” devices to the cloud infrastructure via a secure channel behind a corporate VPN."
+		},
+		"3": {
+			ru: "Возможность предварительной обработки и анализа данных (построение временных рядов, интеграция в Jupyter).",
+			en: "Ability to pre-process and analyze data (building time series, integration into Jupyter)."
+		}
+	},
+	"API-item": {
+		"1": {
+			ru: "Интеграция GET, POST и любых других запросов по требованиям пользователей.",
+			en: "Integration of GET, POST, and any other requests as required by users."
+		},
+		"2": {
+			ru: "Мониторинг API (Grafana).",
+			en: "API monitoring (Grafana)."
+		},
+		"3": {
+			ru: "Защищенный доступ.",
+			en: "Secured access."
+		}
+	},
+	"UVYield-item": {
+		"1": {
+			ru: "Анализ силы и равномерности флуоресценции гранул при УФ облучении.",
+			en: "Analysis of the strength and uniformity of granule fluorescence under UV irradiation."
+		},
+		"2": {
+			ru: "Анализ размера, формы, цвета гранул.",
+			en: "Analysis of the size, shape, and color of granules."
+		},
+		"3": {
+			ru: "Возможность установки устройства в производственные лаборатории.",
+			en: "Possibility of installing the device in production laboratories."
+		}
 	},
 }
 
