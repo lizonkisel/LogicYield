aa3dbfbfbc372c5a844f1b9483035cf63d32ba4e
lizonkisel <ees.hwork@gmail.com>
2024-09-16 22:31:10 +0300

    feat: add menu translation for all pages

--- texts/contactsTexts.js
+++ texts/contactsTexts.js
@@ -1,4 +1,36 @@
 const contactsTexts = {
+	"menu-item": {
+		"1": {
+			ru: "Продукты",
+			en: "Products",
+		},
+		"2": {
+			ru: "Софт",
+			en: "Software",
+		},
+		"3": {
+			ru: "Команда",
+			en: "Team",
+		},
+		"4": {
+			ru: "Контакты",
+			en: "Contacts",
+		}
+	},
+	"team-menu-item": {
+		"1": {
+			ru: "О нас",
+			en: "About us",
+		},
+		"2": {
+			ru: "Наши навыки",
+			en: "Our Expertise",
+		},
+		"3": {
+			ru: "Перспективные проекты",
+			en: "Promising projects",
+		}
+	},
   "title": {
 		ru: "Контакты",
 		en: "Contacts",
