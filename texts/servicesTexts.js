1499459250b559e8d43a6e85b2cf498cf6980aa4
lizonkisel <ees.hwork@gmail.com>
2024-10-20 21:41:07 +0300

    feat: add all translations for Services psge

--- texts/servicesTexts.js
+++ texts/servicesTexts.js
@@ -24,287 +24,287 @@ const servicesTexts = {
   "services__desc-item": {
     "1": {
       ru: "На производстве часто возникают задачи, для которых не существует готовых решений. Либо же такие решения требуют существенной адаптации, исходя из устройства технологичесйких линий и внутренних процессов конкретного цеха или предприятия. Особенно остро эта проблема наблюдается при внедрении цифровых решений.",
-      en: "Products",
+      en: "Manufacturing frequently faces unique challenges that cannot be solved with off-the-shelf solutions. These challenges often necessitate significant customization to fit the specific equipment and processes of individual facilities, particularly when integrating digital technologies.",
     },
     "2": {
       ru: "Наша команда оказывает",
-      en: "Products",
+      en: "Our team offers",
     },
     "3": {
       ru: "Наша компания находится в поиске индустриальных партнёров и инвесторов для участия в грантовых проектах.",
-      en: "Products",
+      en: "Our company is actively seeking industrial partners and investors to collaborate on grant-funded projects.",
     },
     "4": {
       ru: "Если вы являетесь промышленным предприятием и заинтересованы в цифровизации и внедрении искусственного интеллекта в производственные процессы, приглашаем вас к сотрудничеству по следующим грантовым программам:",
-      en: "Products",
+      en: "If you are an industrial enterprise interested in digitalization and the implementation of artificial intelligence in your production processes, we invite you to collaborate on the following grant programs:",
     },
   },
   "title": {
     ru: "Услуги по проведению НИР и НИОКР работ на стыке IT и промышленного производства",
-    en: "Team",
+    en: "R&D services focused on integrating information technology into industrial manufacturing processes",
   },
   "services-subtitle": {
 		"1": {
 			ru: "Задачи, которые мы решаем",
-			en: "About device",
+			en: "The challenges we address",
 		},
 		"2": {
 			ru: "Наши преимущества",
-			en: "Features:",
+			en: "Our advantages",
 		},
 		"3": {
 			ru: "Модели сотрудничества",
-			en: "Industrial trials",
+			en: "Collaboration models",
 		},
 		"4": {
 			ru: "Грантовые работы",
-			en: "Specifications",
+			en: "Grant-funded projects",
 		}
 	},
   "tasksItem-title": {
     "1": {
 			ru: "Разработка ПО на базе компьютерного зрения",
-			en: "About device",
+			en: "Computer vision-based software development",
 		},
 		"2": {
 			ru: "Сборка ПАК",
-			en: "Features:",
+			en: "Assembly of hardware and software complexes",
 		},
 		"3": {
 			ru: "Выполнение научно-исследовательских и грантовых работ",
-			en: "Industrial trials",
+			en: "Conducting research and grant-funded projects",
 		}
   },
   "software-list-item": {
     "1": {
 			ru: "создание и разметка датасетов (собственная инфраструктура для разметки данных);",
-			en: "About device",
+			en: "developing and annotating datasets (proprietary data annotation platform);",
 		},
 		"2": {
 			ru: "разработка и обучение нейронных сетей компьютерного зрения для распознавания любых предметов (от гранулы до человека);",
-			en: "Features:",
+			en: "development and training of computer vision neural networks for recognizing any objects (from a granule to a person);",
 		},
 		"3": {
 			ru: "развертывание сервисов фото- и видеоаналитики: API, мониторинг, облачная инфраструктура;",
-			en: "Industrial trials",
+			en: "deployment of photo and video analytics services: API, monitoring, cloud infrastructure;",
 		},
     "4": {
 			ru: "работа с данными разной природы: изображения, результаты химического и физического анализа, спутниковые, метео- и геоданные.",
-			en: "Industrial trials",
+			en: "processing data of various types, including images, chemical and physical analysis results, satellite imagery, meteorological and geospatial data.",
 		}
   },
   "softHardSys-list-item": {
     ru: "разработка, сборка и шеф-монтаж устройств оптического мониторинга.",
-    en: "About device",
+    en: "development, assembly, and supervision of the installation of optical monitoring devices.",
   },
   "RandD-list-item": {
     "1": {
 			ru: "подготовка документации для регистрации программ для ЭВМ, патентов на изобретение и пр.;",
-			en: "About device",
+			en: "development of application materials for registering computer software, patenting inventions, and other intellectual property;",
 		},
 		"2": {
 			ru: "проведение анализа технологий, обзора решений и консалтинг в сфере промышленного контроля качества, искусственного интеллекта, машинного зрения;",
-			en: "Features:",
+			en: "providing of technology analysis, solution reviews, and consulting for industrial quality control, AI, and machine vision;",
 		},
 		"3": {
 			ru: "выполнение грантовых проектов на базе промышленного партнёра/инвестора.",
-			en: "Industrial trials",
+			en: "carrying out grant-funded projects in collaboration with an industrial partner/investor.",
 		}
   },
   "advantages-item": {
     "1": {
 			ru: "Уникальная комбинация опыта работы в производственной  среде и в IT-компаниях",
-			en: "About device",
+			en: "Unique combination of experience working in industrial environments and IT companies",
 		},
 		"2": {
 			ru: "Научный бэкграунд и понимание естественнонаучной сущности производственных процессов",
-			en: "Features:",
+			en: "Scientific background and understanding of the natural science foundations of industrial processes",
 		},
 		"3": {
 			ru: "Возможность выезда на предприятия для проведения ОПИ",
-			en: "Industrial trials",
+			en: "Ability to conduct industrial trials",
 		},
 		"4": {
 			ru: "Широкий диапазон используемых технологий",
-			en: "Specifications",
+			en: "Wide range of using technologies",
 		}
   },
   "coopModels-title": {
     "1": {
 			ru: "Проведение НИР/НИОКР",
-			en: "About device",
+			en: "Conducting R&D (Research and Development)",
 		},
 		"2": {
 			ru: "Проведение  НИР/НИОКР “под ключ”",
-			en: "Features:",
+			en: "Complete R&D solutions",
 		},
 		"3": {
 			ru: "Разработка ПО “под ключ”",
-			en: "Industrial trials",
+			en: "Complete software development",
 		},
   },
   "science-item": {
     "1": {
 			ru: "Разработка ТЗ.",
-			en: "About device",
+			en: "Development of a technical specification.",
 		},
 		"2": {
 			ru: "Заключение договора об опытно-промышленных испытаниях с целью подтверждения возможности достижения результатов, заявленных в ТЗ (при необходимости).",
-			en: "Features:",
+			en: "Execution of a contract for pilot-scale industrial trials aimed at confirming the feasibility of achieving the results outlined in the technical specification.",
 		},
 		"3": {
 			ru: "Проведение ОПИ.",
-			en: "Industrial trials",
+			en: "Conduct of pilot-scale industrial trials.",
 		},
     "4": {
 			ru: "Заключение договора на НИР/НИОКР.",
-			en: "Industrial trials",
+			en: "Conclusion of a contract for research and development (R&D) work.",
 		}
   },
   "scienceKey-item": {
     "1": {
 			ru: "Разработка ТЗ.",
-			en: "About device",
+			en: "Development of a technical specification.",
 		},
 		"2": {
 			ru: "Заключение договора об опытно-промышленных испытаниях с целью подтверждения возможности достижения результатов, заявленных в ТЗ (при необходимости).",
-			en: "Features:",
+			en: "Execution of a contract for pilot-scale industrial trials aimed at confirming the feasibility of achieving the results outlined in the technical specification.",
 		},
 		"3": {
 			ru: "Проведение ОПИ.",
-			en: "Industrial trials",
+			en: "Conduct of pilot-scale industrial trials.",
 		},
     "4": {
 			ru: "Заключение договора на НИР/НИОКР.",
-			en: "Industrial trials",
+			en: "Conclusion of a contract for research and development (R&D) work.",
 		}
   },
   "software-item": {
     "1": {
 			ru: "Разработка ТЗ.",
-			en: "About device",
+			en: "Development of a technical specification.",
 		},
 		"2": {
 			ru: "Заключение договора на разработку ПО.",
-			en: "Features:",
+			en: "Conclusion of a contract for a software development.",
 		},
 		"3": {
 			ru: "Передача пакета пользовательской документации на ПО, а также исходного кода и прав на интеллектуальную собственность.",
-			en: "Industrial trials",
+			en: "Transfer of a package of user documentation for the software, as well as source code and intellectual property rights.",
 		}
   },
   "rights-text": {
     "1": {
 			ru: "При данной модели сотрудничества все права на объекты интеллектуальной собственности, созданные в рамках НИР/НИОКР остаются за ООО “Лоджик Илд”.",
-			en: "About device",
+			en: "Under this cooperation model, all intellectual property rights created within the framework of the R&D project shall remain the property of LLC “Logic Yield”.",
 		},
 		"2": {
 			ru: "При данной модели сотрудничества все права на объекты интеллектуальной собственности, созданные в рамках НИР/НИОКР переходят заказчику.",
-			en: "Features:",
+			en: "Under this cooperation model, all rights to intellectual property created within the framework of R&D projects are transferred to the customer.",
 		}
   },
   "grants-title": {
 		"1": {
 			ru: "Программа “Развитие” от Фонда Содействия Инновациям",
-			en: "Products",
+			en: "The “Development” Program by The Foundation For Assistance To Small Innovative Enterprises",
 		},
 		"2": {
 			ru: "Гранты на реализацию особо значимых проектов от Фонда “Сколково” (в рамках федеральных проектов «Искусственный интеллект» и “Цифровые технологии”)",
-			en: "Software",
+			en: "Skolkovo Foundation Grants for High-Impact AI and Digital Projects",
 		},
 		"3": {
 			ru: "Программа “Коммерциализация” от Фонда Содействия Инновациям",
-			en: "Team",
+			en: "The “Commercialisation” Program by The Foundation For Assistance To Small Innovative Enterprises",
 		},
 		"4": {
 			ru: "Гранты на реализацию особо значимых проектов по внедрению российских решений в сфере информационных технологий (РФРИТ)",
-			en: "Contacts",
+			en: "Grants for the implementation of especially significant projects for the introduction of Russian solutions in the field of information technologies (by Russian Information Technology Development Fund)",
 		}
   },
   "FASIE1-item": {
 		"1": {
 			ru: "размер гранта – не более 30 млн. рублей (сумма варьируется от 15 до 30 млн. рублей в зависимости от конкурса);",
-			en: "Products",
+			en: "grant amount: Up to 30 million rubles (the amount varies from 15 to 30 million rubles depending on the competition);",
 		},
 		"2": {
 			ru: "срок гранта – не более 24 месяцев (срок варьируется от 12 до 24 месяцев в зависимости от конкурса);",
-			en: "Software",
+			en: "grant duration: Up to 24 months (the duration varies from 12 to 24 months depending on the competition);",
 		},
 		"3": {
 			ru: "внебюджетное софинансирование (из собственных средств или средств инвестора) – не менее 15% от суммы гранта (варьируется в зависимости от конкурса);",
-			en: "Team",
+			en: "non-budgetary co-financing: At least 15% of the grant amount must be provided from own funds or investor funds (varies depending on the competition);",
 		},
 		"4": {
 			ru: "направление расходов – проведение НИОКР.",
-			en: "Contacts",
+			en: "purpose of funding: Research and development (R&D).",
 		}
   },
   "SK-item": {
 		"1": {
 			ru: "размер гранта – от 20 до 100 млн рублей;",
-			en: "Products",
+			en: "grant amount: 20 to 100 million rubles;",
 		},
 		"2": {
 			ru: "получатели – компании, разрабатывающие ИИ-решения для улучшения технологических и бизнес-процессов;",
-			en: "Software",
+			en: "recipients: Companies developing AI solutions to improve technological and business processes;",
 		},
 		"3": {
 			ru: "условие – наличие пилотного проекта, который предусматривает использование ИИ-технологий в производстве, операционной работе или сервисах для приоритетных отраслей экономики.",
-			en: "Team",
+			en: "requirement: The presence of a pilot project that involves the use of AI technologies in production, operations, or services for priority sectors of the economy.",
 		}
   },
   "FASIE2-item": {
 		"1": {
 			ru: "размер гранта – не более 30 млн рублей;",
-			en: "Products",
+			en: "grant amount: Up to 30 million rubles.",
 		},
 		"2": {
 			ru: "срок гранта – 12 месяцев (2 этапа по 6 месяцев);",
-			en: "Software",
+			en: "grant duration: 12 months (consisting of two 6-month stages).",
 		},
 		"3": {
 			ru: "внебюджетное софинансирование (из собственных средств или средств инвестора) – не менее 50% суммы гранта;",
-			en: "Team",
+			en: "non-budgetary co-financing (from own funds or investor funds): at least 50% of the grant amount.",
 		},
 		"4": {
 			ru: "направление расходов – коммерциализация результатов НИОКР.",
-			en: "Contacts",
+			en: "purpose of funding: Commercialization of R&D results.",
 		}
   },
   "RFRIT-item": {
 		"1": {
 			ru: "размер гранта – от 10 млн ₽ до 6 млрд ₽;",
-			en: "Products",
+			en: "grant amount: ranging from 10 million rubles to 6 billion rubles;",
 		},
 		"2": {
 			ru: "срок реализации особо значимого проекта – от 6 до 60 месяцев;",
-			en: "Software",
+			en: "grant duration: between 6 and 60 months;",
 		},
 		"3": {
 			ru: "внебюджетное софинансирование – не менее 50% сметы реализации проекта;",
-			en: "Team",
+			en: "non-budgetary co-financing: at least 50% of the total project cost;",
 		},
 		"4": {
 			ru: "условие – проект одобрен и согласован для включения в перечень особо значимых проектов решением отраслевого комитета/комитета по развитию ПО;",
-			en: "Contacts",
+			en: "requirement: the project must be approved and agreed upon for inclusion in the list of high-priority projects by the relevant industry committee or software development committee.",
 		}
   },
   "grants-moreText": {
 		"1": {
 			ru: "Подробнее",
-			en: "Products",
+			en: "More details",
 		},
 		"2": {
 			ru: "Подробнее",
-			en: "Software",
+			en: "More details",
 		},
 		"3": {
 			ru: "Подробнее",
-			en: "Team",
+			en: "More details",
 		},
 		"4": {
 			ru: "Подробнее",
-			en: "Contacts",
+			en: "More details",
 		}
   },
 }
