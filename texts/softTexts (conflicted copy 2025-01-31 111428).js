2a86c0b8b9127c97ff2979439fc4fc3cf16db0f8
lizonkisel <ees.hwork@gmail.com>
2025-03-03 14:05:07 +0300

    strange commit for wrong rebase fixing

--- texts/softTexts (conflicted copy 2025-01-31 111428).js
+++ texts/softTexts (conflicted copy 2025-01-31 111428).js
@@ -48,14 +48,14 @@ const softTexts = {
 			ru: "DotPulse",
 			en: "DotPulse",
 		},
-		// "3": {
-		// 	ru: "GOA API",
-		// 	en: "GOA API",
-		// },
-		// "4": {
-		// 	ru: "GOA API",
-		// 	en: "GOA API",
-		// },
+		"3": {
+			ru: "GOA API",
+			en: "GOA API",
+		},
+		"4": {
+			ru: "GOA API",
+			en: "GOA API",
+		},
 		"5": {
 			ru: "Нейросетевой анализ номерных знаков автомобилей",
 			en: "Neural network analysis of vehicle license plates",
