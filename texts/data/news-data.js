71795494f3378c99ead1f196c3e2b60760cb1184
lizonkisel <ees.hwork@gmail.com>
2025-09-23 11:20:14 +0300

    feat: add styles and correct image route for news page

---
+++ texts/data/news-data.js
@@ -0,0 +1,32 @@
+const news = [
+  {
+    id: '1753660800',
+    name: "Участие в Летней школе LxMLS 2025 (Португалия, Лиссабон)",
+    topic: "Образование и повышение квалификации",
+    date: "28.07.2025",
+    img_desc: "Иван Иванов",
+    img_author: "Иван Иванов",
+    img_src: "../assets/img/news/Lisbon_title.jpg",
+    content: [
+      'azaza',
+      'Это подробное описание первой новости. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'fdkjfdlk'
+    ],
+  },
+  {
+    id: "1751587200",
+    name: "Участие в конференции EDM (Республика Алтай)",
+    topic: "Выставки и конференции",
+    date: "04.07.2025",
+    img_desc: "Иван Иванов",
+    img_author: "Иван Иванов",
+    img_src: "../assets/img/news/EDM_title.jpeg",
+        content: [
+      'azaza',
+      'Это подробное описание второй новости. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'fdkjfdlk'
+    ],
+  }
+];
+
+export { news };
