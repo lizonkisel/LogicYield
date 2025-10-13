323907c23cc328904401635d61e167b5d6e4548e
lizonkisel <ees.hwork@gmail.com>
2025-10-13 11:56:45 +0300

    feat: add a-tag for text links for oneNews page

--- texts/data/news-data.js
+++ texts/data/news-data.js
@@ -15,11 +15,14 @@ const news = [
     img_src: "../assets/img/news/Lisbon_title.jpg",
     content: [
       {
+        type: "text",
         ru: "Основатель и идеолог нашей команды Юновидов Дмитрий принял участие в работе летней школы машинного обучения Lisbon Machine Learning School (LxMLS 2025), Lisbon, Portugal.",
         en: "The founder and ideologist of our team, Dmitrii Iunovidov, took part in the Lisbon Machine Learning School (LxMLS 2025) in Lisbon, Portugal.",
       },
       // Пока так, потому что одна новость почему-то не отображается
-      { ru: "",
+      { 
+        type: "text",
+        ru: "",
         en: ""
       }
     ],
@@ -40,22 +43,27 @@ const news = [
     img_src: "../assets/img/news/EDM_title.jpeg",
     content: [
       {
+        type: "text",
         ru: "В период с 27 июня по 1 июля наши коллеги Юновидов Дмитрий и Юновидова Елизавета приняли участие в работе конфереции 2025 IEEE EDM (2025 IEEE 26th International Conference of Young Professionals in Electron Devices and Materials).",
         en: "From 27 June to 1 July, our colleagues Dmitrii Iunovidov and Elizaveta Iunovidova took part in the 2025 IEEE EDM conference (2025 IEEE 26th International Conference of Young Professionals in Electron Devices and Materials)",
       },
       {
+        type: "text",
         ru: 'Доклад Дмитрия "A CPU-Efficient Robotic System for Instance Segmentation and Control of Mineral Fertilizer Granulation" занял второе место в секции "Software Engineering and Cyber-Physical Systems".',
         en: "Dmitrii's paper, ‘A CPU-Efficient Robotic System for Instance Segmentation and Control of Mineral Fertiliser Granulation,’ took second place in the ‘Software Engineering and Cyber-Physical Systems’ section",
       },
       {
+        type: "text",
         ru: "Кроме того, в персональном конкурсе «Вовлечение молодых профессионалов» проект Дмитрия занял первое место.",
         en: "In addition, Dmitrii's project took first place in the personal competition ‘Young professionals involvement competition’."
       },
       {
+        type: "text",
         ru: "По результатам конференции планируется публикация лучших статей в Q1-Q4 SCOPUS и Web of science.",
         en: "Based on the results of the conference, the best articles are planned to be published in Q1-Q4 SCOPUS and Web of Science.",
       },
       {
+        type: "text",
         ru: "Помимо научной программы, коллегам удалось отдохнуть, покататься на лошадях и провести время на природе без мерцания синего экрана. С чем мы их и поздравляем!",
         en: "In addition to the scientific programme, our colleagues managed to relax, go horse riding and spend time in nature without the glare of blue screens. We congratulate them on this achievement!’",
       },
@@ -77,10 +85,17 @@ const news = [
     img_src: "../assets/img/news/Belgrade_title.jpg",
     content: [
       {
+        type: "text",
         ru: "Выступление Дмитрия Юновидова на Data Fest (г. Белград) в гостях у Яндекса в секции ML in Manufacturing. Доклад на английском языке на тему: Science Like Industry. MLOps for the Effective R&D Development and Deployment of Computer Vision Models in Industrial Environments.",
         en: "Dmitry Yunovidov delivered an English-language presentation at Data Fest in Belgrade, as a guest of Yandex in the ML in Manufacturing section. The lecture was titled: 'Science Like Industry: MLOps for the Effective R&D Development and Deployment of Computer Vision Models in Industrial Environments.'",
       },
       {
+        type: "text",
+        ru: "Подробнее:",
+        en: "More details:",
+      },
+      {
+        type: "link",
         ru: 'https://www.youtube.com/watch?v=vGOe8b9O1Ek',
         en: 'https://www.youtube.com/watch?v=vGOe8b9O1Ek'
       }
@@ -102,18 +117,27 @@ const news = [
     img_src: "../assets/img/news/Coop-Ammoni_title.jpg",
     content: [
       {
+        type: "text",
         ru: "В телеграм-канале АО «Аммоний» - ООО «Менделеевсказот» и на ресурсе FertilizerDaily вышли статьи о внедрении системы оптического контроля сыпучих материалов «DotPulse» в цехе аммиачной селитры АО «Аммоний», г. Менделеевск, Республика Татарстан.",
         en: "Articles detailing the implementation of the «DotPulse» optical control system for bulk materials have been published. These articles appeared on the JSC «Ammoniy» - LLC «Mendeleevskazot» Telegram channel and the FertilizerDaily resource. The system was introduced in the ammonium nitrate shop of JSC «Ammoni» in Mendeleyevsk, Republic of Tatarstan.",
       },
       {
+        type: "text",
         ru: 'Благодарим коллег за доверие нам и нашим технологиям и надеемся на дальнейшее плодотворное сотрудничество!',
         en: 'We thank our colleagues for their trust in us and our technologies and look forward to further fruitful cooperation!'
       },
       {
+        type: "text",
+        ru: "Подробнее:",
+        en: "More details:",
+      },
+      {
+        type: "link",
         ru: 'https://t.me/ammoni_azot/5310',
         en: 'https://t.me/ammoni_azot/5310'
       },
       {
+        type: "link",
         ru: 'https://www.fertilizerdaily.ru/20250127-ammonij-vnedril-innovaczionnuyu-sistemu-raspoznavaniya-granul/',
         en: 'https://www.fertilizerdaily.ru/20250127-ammonij-vnedril-innovaczionnuyu-sistemu-raspoznavaniya-granul/'
       },
@@ -135,14 +159,17 @@ const news = [
     img_src: "../assets/img/news/First-sale_title.jpg",
     content: [
       {
+        type: "text",
         ru: "Мы долго шли к этому и, вот, свершилось!",
         en: "We have been working towards this for a long time, and now it has finally happened!",
       },
       {
+        type: "text",
         ru: 'Первая единица устройства "DotPulse" не просто прошла серию опытно-промышленных испытаний, но была продана и поставлена на одно из предприятий-производителей минеральных удобрений.',
         en: 'The first unit of the "DotPulse" device achieved a major milestone: it was sold and supplied to a mineral fertilizer manufacturer, following a successful series of pilot industrial trials.'
       },
       {
+        type: "text",
         ru: "Начало положено, впереди новые испытания, технологии и устройства. Мы мечтаем о том, чтобы  контроль качества промышленно производимой продукции становился всё более автоматизированным и прозрачным. И счастливы, что есть возможность внести свой вклад в этот процесс. Вперёд и вверх!",
         en: "The foundation has been laid, and new challenges, technologies, and devices await us. Our goal is for the quality control of industrially produced goods to become ever more automated and transparent. We are delighted to have the chance to contribute to this process. Forward and up!",
       },
@@ -164,10 +191,12 @@ const news = [
     img_src: "../assets/img/news/APMAS_title.jpg",
     content: [
       {
+        type: "text",
         ru: "Наша команда приняла участие в работе конференции APMAS 2024 - 14th International Advances in Applied Physics & Materials Science Congress & Exhibition.",
         en: "Our team took part in the APMAS 2024 conference – the 14th International Advances in Applied Physics & Materials Science Congress & Exhibition.",
       },
       {
+        type: "text",
         ru: 'В рамках мероприятия были представлен девайс "DotPulse", удалось обменяться опытом и обрести новые профессиональные связи в сфере нейронных сетей и промышленного производства.',
         en: 'The event featured the presentation of the "DotPulse" device, and provided an opportunity to exchange experiences and establish new professional connections in the field of neural networks and industrial production.'
       }
